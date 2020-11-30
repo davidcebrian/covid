832a40d42b42ad644f81c4380f804a1933b9f1fa
David <davidcebriancortes0@gmail.com>
2020-11-30 08:18:35 +0100

    covid

--- src/app/services/general-service.service.ts
+++ src/app/services/general-service.service.ts
@@ -12,17 +12,20 @@ export class GeneralService {
 
   constructor ( private http: HttpClient ) { }
 
+  
+  getInfoPaises(): Observable<any>{
+    return this.http.get(this.infoEndP).pipe( map(res =>{
+      return res["Countries"];
+    }))
+  }
+  
+  
   getInfoGeneral(): Observable<any>{
     return this.http.get(this.infoEndP).pipe( map(res => {
       return res["Global"];
     }))
   }
 
-  getInfoPaises(): Observable<any>{
-    return this.http.get(this.infoEndP).pipe( map(res =>{
-      return res["Countries"];
-    }))
-  }
 
   getInfoSpain(): Observable<any>{
     return this.http.get(this.infoEndP).pipe( map(res =>{
